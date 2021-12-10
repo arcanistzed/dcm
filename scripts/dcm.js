5c761849684af8bb598e1de83dc369f9eab97eb0
arcanist <arcanistzed@gmail.com>
2021-12-09 20:34:19 -0500

    feat: DM only option
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- scripts/dcm.js
+++ scripts/dcm.js
@@ -7,15 +7,25 @@ class Dcm {
      */
     constructor() {
         // Register settings
-        Hooks.on("init", () => game.settings.register(Dcm.ID, "invert", {
-            name: "Invert Keybinding",
-            hint: "Only enable the default context menu when the Control key is pressed",
-            scope: "client",
-            config: true,
-            type: Boolean,
-            default: false,
-            onChange: val => { Dcm.pause = val; Dcm.log(false, "settings changed", Dcm.pause) },
-        }));
+        Hooks.on("init", () => {
+            game.settings.register(Dcm.ID, "invert", {
+                name: "Invert Keybinding",
+                hint: "Only enable the default context menu when the Control key is pressed",
+                scope: "client",
+                config: true,
+                type: Boolean,
+                default: false,
+                onChange: val => { Dcm.pause = val; Dcm.log(false, "settings changed", Dcm.pause) },
+            });
+            game.settings.register(Dcm.ID, "dmOnly", {
+                name: "DM Only",
+                hint: "Only enable the default context menu for DMs",
+                scope: "world",
+                config: true,
+                type: Boolean,
+                default: false,
+            });
+        });
 
         // Register for DevMode
         Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
@@ -57,10 +67,11 @@ class Dcm {
         Hooks.on("init", async () => {
             // Default value
             Dcm.pause = !!game.settings.get(Dcm.ID, "invert");
+            this.registerKeybindings();
 
-            // Show the context menu, depending on if it's paused or not
+            // Show the context menu, depending on if it's paused or not and if it's DM only
             document.addEventListener("contextmenu", event => {
-                if (!Dcm.pause && game.user.isGM) event.stopPropagation();
+                if (!Dcm.pause && (!game.settings.get("dcm", "dmOnly") || game.user.isGM)) event.stopPropagation();
                 Dcm.log(false, "contextmenu", !Dcm.pause);
             }, true);
         });
